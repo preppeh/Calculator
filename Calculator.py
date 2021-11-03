6912ea471f0c9f52a84cae2b5f3e3591536299d2
Amanda Tebo <tebo.amanada@yahoo.com>
2021-11-02 18:29:12 -0700

    Added basic functionality to buttons. Need to implement shunting yard and evaluate

--- Calculator.py
+++ Calculator.py
@@ -4,9 +4,13 @@
 #Calculator Class
 
 from tkinter import *
+import math
+
 
 class Calculator:
     def __init__(self, master):
+        self.openParenCounter = 0
+        self.closeParenCounter = 0
         self.master = master
         master.title("Calculator")
         self.equation = Entry(master, width = 36, borderwidth = 5)
@@ -14,6 +18,7 @@ class Calculator:
         self.createButton()
         
     def createButton(self):
+        #creation of the buttons to be used
         b0 = self.addButton(0)
         b1 = self.addButton(1)
         b2 = self.addButton(2)
@@ -29,7 +34,7 @@ class Calculator:
         b_mult = self.addButton('*')
         b_div = self.addButton('/')
         b_clear = self.addButton('c')
-        b_equal = self.addButton('+')
+        b_equal = self.addButton('=')
         b_sin = self.addButton('sin')
         b_cos = self.addButton('cos')
         b_tan = self.addButton('tan')
@@ -42,27 +47,72 @@ class Calculator:
         b_sqrt = self.addButton('sqrt')
         b_posneg = self.addButton('+/-')
         b_decimal = self.addButton('.')
+        b_openParenthesis = self.addButton('(')
+        b_closeParenthesis = self.addButton(')')
+        b_bcksp = self.addButton('bcksp')
 
-        row1=[b_sin, b_cos, b_tan, b_clear]
-        row2 = [b_arcsin, b_arccos, b_arctan, b_sqrt]
-        row3 = [b_log, b_ln, b_expon, b_add]
-        row4 = [b7, b8, b9, b_sub]
-        row5 = [b4, b5, b6, b_mult]
-        row6 = [b1, b2, b3, b_div]
-        row7 = [b_posneg, b0, b_decimal, b_equal]
+        #layout of the buttons
+        row1 = [b_bcksp, b_openParenthesis, b_closeParenthesis]
+        row2 = [b_sin, b_cos, b_tan, b_clear]
+        row3 = [b_arcsin, b_arccos, b_arctan, b_sqrt]
+        row4 = [b_log, b_ln, b_expon, b_add]
+        row5 = [b7, b8, b9, b_sub]
+        row6 = [b4, b5, b6, b_mult]
+        row7 = [b1, b2, b3, b_div]
+        row8 = [b_posneg, b0, b_decimal, b_equal]
 
+        #cycle through layout arrays and create the button grid
         r = 1
-        for row in [row1, row2, row3, row4, row5, row6, row7]:
+        for row in [row1, row2, row3, row4, row5, row6, row7, row8]:
             c = 0
             for buttn in row:
                 buttn.grid(row=r, column = c, columnspan = 1)
                 c+=1
             r+=1
 
+    
+
     def addButton(self, value):
-        return Button(self.master, text = value, width = 9)
+        return Button(self.master, text=value, width=9, command = lambda: self.clickButton(str(value)))
 
+    def clickButton(self, value):
+        current_equation = str(self.equation.get())
+        if value == 'c':
+            self.equation.delete(0, END)
+        
+        elif value == '=':
+            if self.closeParenCounter != self.openParenCounter:
+                self.equation = "Invalid parenthesis"
+                self.clickButton('c')
+                self.closeParenCounter = 0
+                self.openParenCounter = 0
+                return
+            answer = str(eval(current_equation))
+            #answer = evaluate(answer)
+            self.equation.delete(-1, END)
+            self.equation.insert(0, answer)
+        
+        elif value == '(':
+            self.openParenCounter += 1
+            self.equation.delete(0, END)
+            self.equation.insert(0, current_equation + value)
 
+        elif value == ')':
+            self.closeParenCounter += 1
+            self.equation.delete(0, END)
+            self.equation.insert(0, current_equation + value)
+
+        #elif value == 'bcksp':
+        #    self.equation = self.equation[:-1]
+        #    current_equation.set(self.equation)
+
+        else:
+            self.equation.delete(0, END)
+            self.equation.insert(0, current_equation + value)
+
+    #def shuntYard(self, equation):
+    #def evaluate(self, equation):
+        
 if __name__ == '__main__':
     root = Tk()
     my_gui = Calculator(root)
